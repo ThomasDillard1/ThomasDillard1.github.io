1ffa761e711173528f59585cf809d2af59468918
Thomas <thomasdillard1@gmail.com>
2023-02-03 19:20:35 -0500

    getting rid of old and misplaced files

--- assets/js/earnings.js
+++
@@ -1,8 +0,0 @@
-//Fetch the api
-try{
-    const res = await fetch("https://www.alphavantage.co/query?function=EARNINGS_CALENDAR&horizon=3month&apikey=NYQ6Y3MD56WEE60B");
-    const data = await res.json();
-    console.log(data)
-} catch (e) {
-    console.error(e);
-}
