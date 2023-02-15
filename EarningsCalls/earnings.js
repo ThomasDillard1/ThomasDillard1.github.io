53cdb93253bf621063dcf48401bcba6770e46aac
Thomas <thomasdillard1@gmail.com>
2023-02-14 19:01:47 -0500

    rename packages

--- EarningsCalls/earnings.js
+++ EarningsCalls/earnings.js
@@ -2,10 +2,10 @@
 //The api is not json, its is actually csv data
 const earnings_calendar = "https://www.alphavantage.co/query?function=EARNINGS_CALENDAR&horizon=3month&apikey=NYQ6Y3MD56WEE60B";
 
-import pkg from 'scramjet';
-const { StringStream } = pkg;
-import pack from 'request';
-const { get } = pack;
+import scramjet from 'scramjet';
+const { StringStream } = scramjet;
+import r from 'request';
+const { get } = r;
 
 // replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
 get(earnings_calendar)
